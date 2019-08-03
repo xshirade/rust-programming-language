939272c193817026eb00399c954b355ae244035e
Satoshi Hirade <shirade222@gmail.com>
2019-08-03 15:34:23 +0900

    Done 4.2

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Learning ["Rust Programming Language"](https://doc.rust-lang.org/book/)
 
 ## Work Summary
 
-| Chapter                                                            | Date       | Comment |
-| ------------------------------------------------------------------ | ---------- | ------- |
+| Chapter                                                            |    Date    | Comment |
+| ------------------------------------------------------------------ | :--------: | ------- |
 | CH01 - Getting Started                                             | 2019/08/02 |         |
 | CH02 - Programming a Guessing Game                                 | 2019/08/02 |         |
 | CH03 - Common Programming Language                                 | 2019/08/03 |         |
-| CH04 - Understanding Ownership                                     | -          |         |
+| CH04 - Understanding Ownership                                     |     -      |         |
 | CH04-01 - What is Ownership?                                       | 2019/08/03 |         |
-| CH04-02 - References and Borrowing                                 | 2019/08/dd |         |
+| CH04-02 - References and Borrowing                                 | 2019/08/03 |         |
 | CH04-03 - The Slice Type                                           | 2019/08/dd |         |
 | CH05 - Using Structs to Structure Related Data                     | 2019/08/dd |         |
 | CH06 - Enums and Pattern Matching                                  | 2019/08/dd |         |
--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -44,11 +44,52 @@ fn main() {
         // println!("s1 = {}, s2 = {}, s3 = {}", s1, s2, s3); // s2 is already moved
     }
 
+    // {
+    //     let s1 = String::from("Calculate String Length");
+    //     let (s2, len) = calculate_length(s1);
+    //     println!("The length of '{}' is {}.", s2, len);
+    // }
+
     {
-        let s1 = String::from("Calculate String Length");
-        let (s2, len) = calculate_length(s1);
-        println!("The length of '{}' is {}.", s2, len);
+        let s1 = String::from("Thw ownership is not moved.");
+        let len = calculate_length(&s1);
+        println!("The length of '{}' is {}.", s1, len);
     }
+
+    {
+        let mut s = String::from("hello");
+        change(&mut s);
+    }
+
+    {
+        let mut s = String::from("hello");
+        {
+            let r1 = &mut s;
+            // let r2 = &mut s; // why this does not output error ? errors when r1 or r2 used
+            // println!("{}, {}", r1, r2);
+        }
+        let r2 = &mut s;
+    }
+
+    {
+        let mut s = String::from("hello");
+        let r1 = &s;
+        let r2 = &s;
+        println!("{}, {}", r1, r2);
+        // this is okay, r3 is declared after r1 and r2 used
+        let r3 = &mut s;
+        println!("{}", r3);
+    }
+
+    {
+        let refernce_to_nothing = danlge();
+    }
+}
+
+fn danlge() -> String {
+    let s = String::from("hello");
+    // &s // this cause error, &s refers to s which is dropped after return &s
+    s
 }
 
 fn takes_ownership(some_string: String) {
@@ -68,7 +109,15 @@ fn takes_and_gives_back(a_string: String) -> String {
     a_string
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
-    (s, length)
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len();
+//     (s, length)
+// }
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
